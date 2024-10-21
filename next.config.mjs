3afd0cb01098535f50fa6117214064c431b0ef58
Bartosz Bednarski <bambocha1997@gmail.com>
2024-10-21 18:10:18 +0200

    Updated next.config.

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,10 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  webpack: (config) => {
+    config.resolve.alias["@"] = path.resolve(__dirname);
+    return config;
+  },
 };
 
 export default nextConfig;
