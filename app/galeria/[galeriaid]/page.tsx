b8c60fb58b63ef7afadfd790178c01be40fe9495
Bartosz Bednarski <bambocha1997@gmail.com>
2024-10-22 15:19:39 +0200

    Update .gitignore to ignore out directory on prod branch

--- app/galeria/[galeriaid]/page.tsx
+++ app/galeria/[galeriaid]/page.tsx
@@ -3,16 +3,27 @@ import { GALLERY_MAIN } from "../../../utils/Gallery/Gallery";
 import GalleryPhotos from "../../../components/Gallery/GalleryPhotos/GalleryPhotos";
 import Scrap2 from "../../../components/Ui/Scrap/Scrap2";
 
+// Generowanie statycznych ścieżek
+export async function generateStaticParams() {
+  return GALLERY_MAIN.map((item) => ({
+    galeriaid: item.href,
+  }));
+}
+
+// Strona dla dynamicznego id galerii
 const GalleryPhotosPage = ({ params }: { params: { galeriaid: string } }) => {
   const data = GALLERY_MAIN.find((item) => item.href === params.galeriaid);
-  if (data !== undefined) {
-    return (
-      <main className={styles.main}>
-        <GalleryPhotos data={data} />
-        <Scrap2 color="darkBlue" position="bottom" />
-      </main>
-    );
+
+  if (data === undefined) {
+    return <p>Galeria nie została znaleziona</p>;
   }
+
+  return (
+    <main className={styles.main}>
+      <GalleryPhotos data={data} />
+      <Scrap2 color="darkBlue" position="bottom" />
+    </main>
+  );
 };
 
 export default GalleryPhotosPage;
--- next.config.mjs
+++ next.config.mjs
@@ -3,17 +3,47 @@ import path from "path";
 import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
 const __dirname = path.dirname(__filename); // get the name of the directory
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: false,
+//   sassOptions: {
+//     includePaths: [path.join(__dirname, "styles")],
+//   },
+//   webpack: (config, { isServer }) => {
+//     // Definiowanie aliasu "@" na główny katalog projektu
+//     config.resolve.alias["@"] = path.resolve(__dirname);
+
+//     // Jeśli nie jest to serwer, wyłącz wielowątkowość dla Webpacka
+//     if (!isServer) {
+//       config.optimization.splitChunks = {
+//         cacheGroups: {
+//           default: false,
+//         },
+//       };
+//       config.optimization.minimizer = [];
+//     }
+
+//     return config;
+//   },
+// };
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
-  swcMinify: false,
+  swcMinify: true, // Ustawienie na true
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
   webpack: (config, { isServer }) => {
-    // Definiowanie aliasu "@" na główny katalog projektu
     config.resolve.alias["@"] = path.resolve(__dirname);
 
-    // Jeśli nie jest to serwer, wyłącz wielowątkowość dla Webpacka
+    // Możesz usunąć lub skomentować poniższą sekcję
     if (!isServer) {
       config.optimization.splitChunks = {
         cacheGroups: {
@@ -25,7 +55,6 @@ const nextConfig = {
 
     return config;
   },
-  swcMinify: false, // Wyłączenie SWC Minifier
 };
 
 export default nextConfig;
