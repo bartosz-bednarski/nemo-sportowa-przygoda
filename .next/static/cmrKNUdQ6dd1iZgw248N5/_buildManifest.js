778e8df0f5898fd7d425eb82b295f901c3309887
Bartosz Bednarski <bambocha1997@gmail.com>
2024-11-04 10:15:45 +0100

    Console.log for navBtn to check on IOS.

---
+++ .next/static/cmrKNUdQ6dd1iZgw248N5/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-7b3be3b0482dca6c.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
