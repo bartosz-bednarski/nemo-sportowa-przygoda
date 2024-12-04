713e16fb77c2334da499acefb3d0db426ae87af3
Bartosz Bednarski <bambocha1997@gmail.com>
2024-12-04 08:37:40 +0100

    Updated sitemap.

--- sitemap.ts
+++ sitemap.ts
@@ -57,6 +57,18 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: "https://nemosportowaprzygoda.pl/ofertazimowa/polkolonie/bialeszalenstwo/korzenna",
     },
     {
+      url: "https://nemosportowaprzygoda.pl/ofertazimowa/weekend-na-stoku/termin-1",
+    },
+    {
+      url: "https://nemosportowaprzygoda.pl/ofertazimowa/weekend-na-stoku/termin-2",
+    },
+    {
+      url: "https://nemosportowaprzygoda.pl/ofertazimowa/weekend-na-stoku/termin-3",
+    },
+    {
+      url: "https://nemosportowaprzygoda.pl/ofertazimowa/weekend-na-stoku/termin-4",
+    },
+    {
       url: "https://nemosportowaprzygoda.pl/naszzespol",
     },
     {
