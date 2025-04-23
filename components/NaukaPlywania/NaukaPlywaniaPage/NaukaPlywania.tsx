92425894f10caa84166d64f451b789b978c1dcda
Bartosz Bednarski <bambocha1997@gmail.com>
2025-04-23 18:42:00 +0200

    Updated menu fonts and implemented infocards at swimming page.

--- components/NaukaPlywania/NaukaPlywaniaPage/NaukaPlywania.tsx
+++ components/NaukaPlywania/NaukaPlywaniaPage/NaukaPlywania.tsx
@@ -6,7 +6,6 @@ import {poolsType} from '@/types/NaukaPlywania/naukaPlywania';
 import {COLORS} from '@/utils/UI/colors';
 import Groups, {GroupsPropsType} from '../Ui/Groups/Groups';
 import {customColors} from '@/types/UI/colors';
-import StickerH4 from '@/components/Ui/Headers/Stickers/StickerH4/StickerH4';
 import NaukaPlywaniaAbout, {
   NaukaPływaniaAboutPropsType,
 } from '../Ui/NaukaPlywaniaAbout/NaukaPlywaniaAbout';
@@ -17,6 +16,115 @@ export interface NaukaPlywaniaPropsType {
   groups?: GroupsPropsType;
   backgroundColor: customColors;
 }
+
+const INFO_CARDS_DATA = [
+  {
+    header: 'KORZYŚCI Z ZAJĘĆ W WODZIE',
+    listItems: [
+      {
+        header: 'Rozwój Psychofizyczny:',
+        text: 'Zajęcia w wodzie wspierają koordynację ruchową, poprawiają napięcie mięśniowe i pomagają w stymulacji zmysłów, co korzystnie wpływa na rozwój malucha.',
+      },
+      {
+        header: 'Wspomaganie Odruchów:',
+        text: 'Wczesny kontakt z wodą podtrzymuje naturalne odruchy niemowlęce, takie jak kopanie czy wstrzymywanie oddechu, co ułatwia adaptację do nowego środowiska.',
+      },
+      {
+        header: 'Budowanie Więzi:',
+        text: 'Zajęcia umożliwiają rodzicom i dzieciom wspólne pokonywanie wyzwań i dzielenie się radością z postępów, co wzmacnia więzi emocjonalne.',
+      },
+      {
+        header: 'Zdrowotne Korzyści:',
+        text: 'Regularne uczestnictwo w zajęciach wzmacnia odporność, poprawia ukrwienie i pomaga redukować dolegliwości, takie jak kolki czy napięcia mięśniowe.',
+      },
+    ],
+  },
+  {
+    header: 'PRZYGOTOWANIE DO ZAJĘĆ',
+    listItems: [
+      {
+        header: 'Spokój i Wyspanie:',
+        text: 'Dziecko powinno być wypoczęte i spokojne przed zajęciami, by mogło w pełni czerpać radość z nowych doświadczeń.',
+      },
+      {
+        header: 'Dopasowanie Godziny:',
+        text: 'Wybierzcie godzinę zajęć, która nie koliduje z drzemką malucha, co pozwoli mu być wypoczętym i gotowym do nauki.',
+      },
+      {
+        header: 'Karmienie:',
+        text: 'Zaleca się karmienie dziecka na około 30 minut przed zajęciami (lub karmienie piersią tuż przed), aby zapewnić mu odpowiednią sytość. Możliwe są też przerwy na karmienie w trakcie zajęć, gdy dziecko tego potrzebuje.',
+      },
+    ],
+  },
+  {
+    header: 'ZAWARTOŚĆ TORBY DZIECKA',
+    listItems: [
+      {header: '', text: 'Ręcznik z kapturkiem i zapasowy ręcznik.'},
+      {header: '', text: 'Wodoodporne pieluszki lub majteczki neoprenowe.'},
+      {header: '', text: 'Zapasowe ubranko po zajęciach.'},
+      {header: '', text: 'Zwykłe pieluszki oraz chusteczki nawilżające.'},
+      {
+        header: '',
+        text: 'Kosmetyki do mycia i pielęgnacji (te używane w domu).',
+      },
+      {header: '', text: 'Ulubiona zabawka dla malucha.'},
+      {header: '', text: 'Przekąska po zajęciach.'},
+    ],
+  },
+  {
+    header: 'ZAWARTOŚĆ TORBY RODZICA',
+    listItems: [
+      {header: '', text: 'Ręcznik dla siebie.'},
+      {header: '', text: 'Przylegający strój kąpielowy.'},
+      {header: '', text: 'Okulary pływackie.'},
+      {header: '', text: 'Klapki basenowe.'},
+      {header: '', text: 'Obuwie zamienne.'},
+      {header: '', text: 'Kosmetyki do mycia po zajęciach.'},
+    ],
+  },
+  {
+    header: 'PODSTAWOWE INFORMACJE',
+    listItems: [
+      {
+        header: 'Wiek dziecka:',
+        text: 'Zajęcia są dedykowane dzieciom od 3. miesiąca życia, które stabilnie utrzymują główkę.',
+      },
+      {
+        header: 'Zgoda lekarza:',
+        text: 'Zalecamy przed zapisaniem dziecka na zajęcia, należy uzyskać zgodę pediatry, aby upewnić się, że nie ma przeciwwskazań do pływania.',
+      },
+      {
+        header: 'Temperatura wody:',
+        text: 'Woda na zajęciach jest podgrzewana do 30-33°C, co zapewnia komfort i bezpieczeństwo dla niemowląt.',
+      },
+      {
+        header: 'Nieobecności:',
+        text: 'W przypadku niemożności uczestnictwa w zajęciach, należy zgłosić nieobecność co najmniej 12 godzin przed zajęciami poprzez system ActiveNow.',
+      },
+    ],
+  },
+  {
+    header: 'BEZPIECZEŃSTWO I KOMFORT',
+    listItems: [
+      {
+        header: 'Wsparcie Instruktora:',
+        text: 'Na każdych zajęciach instruktorzy pomagają rodzicom, pokazując, jak bezpiecznie trzymać dziecko i ułatwić mu adaptację do nowego środowiska.',
+      },
+      {
+        header: 'Elementy Samoratownictwa:',
+        text: 'Program obejmuje także naukę podstawowych umiejętności związanych z bezpieczeństwem w wodzie, takich jak świadome wstrzymywanie oddechu i pierwsze zanurzenia.',
+      },
+      {
+        header: 'Bliskość i Zaufanie:',
+        text: 'Dziecko w nowym środowisku najbardziej potrzebuje poczucia bezpieczeństwa, dlatego na zajęciach szczególny nacisk kładziemy na bliskość rodzica i jego wsparcie.',
+      },
+      {
+        header: '',
+        text: 'Zajęcia pływackie dla niemowląt to nie tylko świetny sposób na aktywne spędzanie czasu, ale także okazja do wsparcia kluczowych umiejętności rozwojowych Twojego dziecka. Razem z nami możecie odkrywać radość i korzyści płynące z przygód w wodzie, już od pierwszych miesięcy życia!',
+      },
+    ],
+  },
+];
 
 const NaukaPlywania = ({
   about,
@@ -36,165 +144,28 @@ const NaukaPlywania = ({
       />
       {about.stickerH2.titleStrip === 'Z RODZICAMI' && (
         <div className={styles.rowBox} id="tips">
-          <div className={styles.columnBox}>
-            <StickerH4
-              title="KORZYŚCI Z ZAJĘĆ"
-              titleStrip="W WODZIE"
-              stripBgColor="basicOrange"
-              stripTextColor="darkBlue"
-            />
-            <ul>
-              <li>
-                <b>Rozwój Psychofizyczny:</b> Zajęcia w wodzie wspierają
-                koordynację ruchową, poprawiają napięcie mięśniowe i pomagają w
-                stymulacji zmysłów, co korzystnie wpływa na rozwój malucha.
-              </li>
-              <li>
-                <b>Wspomaganie Odruchów:</b> Wczesny kontakt z wodą podtrzymuje
-                naturalne odruchy niemowlęce, takie jak kopanie czy
-                wstrzymywanie oddechu, co ułatwia adaptację do nowego
-                środowiska.
-              </li>
-              <li>
-                <b>Budowanie Więzi:</b> Zajęcia umożliwiają rodzicom i dzieciom
-                wspólne pokonywanie wyzwań i dzielenie się radością z postępów,
-                co wzmacnia więzi emocjonalne.
-              </li>
-              <li>
-                <b>Zdrowotne Korzyści:</b> Regularne uczestnictwo w zajęciach
-                wzmacnia odporność, poprawia ukrwienie i pomaga redukować
-                dolegliwości, takie jak kolki czy napięcia mięśniowe.
-              </li>
-            </ul>
-          </div>
-          <div className={styles.columnBox}>
-            <StickerH4
-              title="PRZYGOTOWANIE "
-              titleStrip="DO ZAJĘĆ"
-              stripBgColor="basicOrange"
-              stripTextColor="darkBlue"
-            />
-            <ul>
-              <li>
-                <b>
-                  Przygotowując się do zajęć na basenie, warto zadbać o kilka
-                  kluczowych aspektów, aby zapewnić komfort dziecku i sobie.
-                </b>
-              </li>
-              <li>
-                <b>Spokój i Wyspanie:</b> Dziecko powinno być wypoczęte i
-                spokojne przed zajęciami, by mogło w pełni czerpać radość z
-                nowych doświadczeń.
-              </li>
-              <li>
-                <b>Dopasowanie Godziny:</b> Wybierzcie godzinę zajęć, która nie
-                koliduje z drzemką malucha, co pozwoli mu być wypoczętym i
-                gotowym do nauki.
-              </li>
-              <li>
-                <b>Karmienie: </b>Zaleca się karmienie dziecka na około 30 minut
-                przed zajęciami (lub karmienie piersią tuż przed), aby zapewnić
-                mu odpowiednią sytość. Możliwe są też przerwy na karmienie w
-                trakcie zajęć, gdy dziecko tego potrzebuje.
-              </li>
-            </ul>
-          </div>
-          <div className={styles.columnBox}>
-            <StickerH4
-              title="ZAWARTOŚĆ TORBY"
-              titleStrip="DZIECKA"
-              stripBgColor="basicOrange"
-              stripTextColor="darkBlue"
-            />
-            <ul>
-              <li>Ręcznik z kapturkiem i zapasowy ręcznik.</li>
-              <li>Wodoodporne pieluszki lub majteczki neoprenowe.</li>
-              <li>Zapasowe ubranko po zajęciach.</li>
-              <li>Zwykłe pieluszki oraz chusteczki nawilżające.</li>
-              <li>Kosmetyki do mycia i pielęgnacji (te używane w domu).</li>
-              <li>Ulubiona zabawka dla malucha.</li>
-              <li>Przekąska po zajęciach.</li>
-            </ul>
-          </div>
-          <div className={styles.columnBox}>
-            <StickerH4
-              title="ZAWARTOŚĆ TORBY"
-              titleStrip="RODZICA"
-              stripBgColor="basicOrange"
-              stripTextColor="darkBlue"
-            />
-            <ul>
-              <li>Ręcznik dla siebie.</li>
-              <li>Przylegający strój kąpielowy.</li>
-              <li>Okulary pływackie.</li>
-              <li>Klapki basenowe.</li>
-              <li>Obuwie zamienne.</li>
-              <li>Kosmetyki do mycia po zajęciach.</li>
-            </ul>
-          </div>
-          <div className={styles.columnBox}>
-            <StickerH4
-              title="PODSTAWOWE"
-              titleStrip="INFORMACJE"
-              stripBgColor="basicOrange"
-              stripTextColor="darkBlue"
-            />
-            <ul>
-              <li>
-                <b>Wiek dziecka:</b> Zajęcia są dedykowane dzieciom od 3.
-                miesiąca życia, które stabilnie utrzymują główkę.
-              </li>
-              <li>
-                <b>Zgoda lekarza:</b> Zalecamy przed zapisaniem dziecka na
-                zajęcia, należy uzyskać zgodę pediatry, aby upewnić się, że nie
-                ma przeciwwskazań do pływania.
-              </li>
-              <li>
-                <b>Temperatura wody:</b> Woda na zajęciach jest podgrzewana do
-                30-33°C, co zapewnia komfort i bezpieczeństwo dla niemowląt.
-              </li>
-              <li>
-                <b>Nieobecności:</b> W przypadku niemożności uczestnictwa w
-                zajęciach, należy zgłosić nieobecność co najmniej 12 godzin
-                przed zajęciami poprzez system ActiveNow.
-              </li>
-            </ul>
-          </div>
-          <div className={styles.columnBox}>
-            <StickerH4
-              title="BEZPIECZEŃSTWO"
-              titleStrip="I KOMFORT"
-              stripBgColor="basicOrange"
-              stripTextColor="darkBlue"
-            />
-            <ul>
-              <li>
-                <b>Wsparcie Instruktora:</b> Na każdych zajęciach instruktorzy
-                pomagają rodzicom, pokazując, jak bezpiecznie trzymać dziecko i
-                ułatwić mu adaptację do nowego środowiska.
-              </li>
-              <li>
-                <b>Elementy Samoratownictwa:</b> Program obejmuje także naukę
-                podstawowych umiejętności związanych z bezpieczeństwem w wodzie,
-                takich jak świadome wstrzymywanie oddechu i pierwsze zanurzenia.
-              </li>
-              <li>
-                <b>Bliskość i Zaufanie:</b> Dziecko w nowym środowisku
-                najbardziej potrzebuje poczucia bezpieczeństwa, dlatego na
-                zajęciach szczególny nacisk kładziemy na bliskość rodzica i jego
-                wsparcie.
-              </li>
-              <li>
-                <b>
-                  Zajęcia pływackie dla niemowląt to nie tylko świetny sposób na
-                  aktywne spędzanie czasu, ale także okazja do wsparcia
-                  kluczowych umiejętności rozwojowych Twojego dziecka. Razem z
-                  nami możecie odkrywać radość i korzyści płynące z przygód w
-                  wodzie, już od pierwszych miesięcy życia!
-                </b>
-              </li>
-            </ul>
-          </div>
+          {INFO_CARDS_DATA.map((cardItem, index) => (
+            <div key={index} className={styles.cardBox}>
+              <h4>{cardItem.header}</h4>
+              <ul>
+                {cardItem.listItems.map((listItem, index) => (
+                  <li key={index}>
+                    {listItem.header !== '' && (
+                      <p className={styles.header}>{listItem.header}</p>
+                    )}
+                    <p
+                      className={styles.text}
+                      style={{
+                        textAlign: listItem.header === '' ? 'center' : 'left',
+                      }}
+                    >
+                      {listItem.text}
+                    </p>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
         </div>
       )}
 
