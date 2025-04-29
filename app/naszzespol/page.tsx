74cf77b47b5259159c37e3b66d994b81a5998e6d
Bartosz Bednarski <bambocha1997@gmail.com>
2025-04-29 08:43:40 +0200

    Updated meta for /naszzespol.

--- app/naszzespol/page.tsx
+++ app/naszzespol/page.tsx
@@ -3,6 +3,20 @@ import styles from '../page.module.scss';
 import HeroTeam from '@/components/NaszZespol/Hero/Hero';
 import InstructorsList from '@/components/NaszZespol/InstructorsList/InstructorsList';
 import Scrap2 from '@/components/Ui/Scrap/Scrap2';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Poznajcie nasz zespół!',
+  description:
+    'Nasz zespół to grono doświadczonych instruktorów, którzy od lat z pasją i zaangażowaniem szkolą w zakresie pływania.',
+  metadataBase: new URL('https://nemosportowaprzygoda.pl/naszzespol'),
+  alternates: {
+    canonical: 'https://nemosportowaprzygoda.pl/naszzespol',
+    languages: {
+      'pl-PL': '/pl-PL',
+    },
+  },
+};
 
 const NaszZespolPage = () => {
   return (
